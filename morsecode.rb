b3ffbc4dc170bcfb67a1530b77a23d7c1cc2edd8
Hans Zizold <hzizold@hotmail.com>
2022-08-24 17:18:53 -0400

    Linter correction

--- morsecode.rb
+++ morsecode.rb
@@ -1,30 +1,32 @@
 def decode_char(str)
-  morsecode  = { ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E", "..-." => "F", "--." => "G", "...." => "H", ".." => "I", ".---" => "J", "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O", ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T", "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y", "--.." => "Z"}
-  return morsecode[str]
+  morsecode = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
+                '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
+                '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
+                '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z' }
+  morsecode[str]
 end
 
 def decode_word(str)
-  letters  = str.split(' ')
+  letters = str.split
   word = ''
-  letters.each{|letter| word+=decode_char(letter)}
-  return word
+  letters.each { |letter| word += decode_char(letter) }
+  word
 end
 
 def decode(str)
   words = str.split('   ')
   text = ''
-  words.each{
-    |word| text += decode_word(word)
-    text+=' '
-  }
-  return text
+  words.each do |word|
+    text += decode_word(word)
+    text += ' '
+  end
+  text
 end
 
-puts decode_char("-..")
+puts decode_char('-..')
 
-puts decode_word("-- -.--")
+puts decode_word('-- -.--')
 
-puts decode("-- -.--   -. .- -- .")
+puts decode('-- -.--   -. .- -- .')
 
-puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
-
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
